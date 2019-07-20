4006410a397d693c84b93c22064e5772688309ad
danielm2402 <damunoz216@unicauca.edu.co>
2019-07-20 15:24:26 -0500

    JWT implentado~

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,5 +1,7 @@
 const db = require("../db/connection");
 const evalidator = require("email-validator");
+const md5=require("md5");
+const jwt = require("jsonwebtoken");
 
 
 module.exports = {
@@ -38,12 +40,12 @@ module.exports = {
                     res.status(502).send(error);
                 }
                 if (result['rowCount'] > 0) {
-                    res.status(502).send("email ya pertenece a otro usuario");
+                    res.status(502).send("Este Email Ya está registrado");
                 } else {
                     console.log("INSERT INTO USUARIO(UNOMBRE, UAPELLIDO, UCORREO, UCONTRASENA, UMETODO) VALUES($1,$2,$3,$4,$5)",
-                    [nombre, apellido, correo, contrasena, 'MD5'])
-                    db.query("INSERT INTO USUARIO(UNOMBRE, UAPELLIDO, UCORREO, UCONTRASENA, UMETODO) VALUES($1,$2,$3,$4,$5)",
-                        [nombre, apellido, correo, contrasena, 'MD5'], (error, result, fields) => {
+                    [nombre, apellido, correo, 'MD5'+'('+contrasena+')','MD5'])
+                    db.query("INSERT INTO USUARIO(UNOMBRE, UAPELLIDO, UCORREO, UCONTRASENA, UMETODO) VALUES($1,$2,$3,MD5($4),$5)",
+                        [nombre, apellido, correo,contrasena,'MD5'], (error, result, fields) => {
                             if (error) {
                                 res.status(502).send(error);
                             }
@@ -56,7 +58,58 @@ module.exports = {
             })
 
         }
-    }
+    },
+
+    login: (req, res)=>{
+        const {
+            correo,
+            contrasena
+        } = req.body;
+
+        if(evalidator.validate(correo)){
+            console.log("SELECT * FROM USUARIO WHERE ucorreo="+"'"+ correo+"'"+" and ucontrasena="+"'"+md5(contrasena)+"'");
+            db.query("SELECT * FROM USUARIO WHERE ucorreo="+"'"+ correo+"'"+" and ucontrasena="+"'"+md5(contrasena)+"'", (error, result, field)=>{
+                if(error){
+                    res.status(502).send(error);
+                }
+                if(result['rowCount'] > 0){
+                    const token = jwt.sign(correo, 'tripcost')
+                    res.json({
+                        success: true,
+                        message: 'token generado',
+                        token,
+                        result
+                    })
+                }
+                else{
+                    res.send("Email o Contraseña incorrecta");
+                }
+                    
+            } )
+        }
+    },
+
+    middleware:((req, res, next)=>{
+        const token=req.body.token || req.query.token || req.headers['x-access-token'];
+        if(token){
+            jwt.verify(token, 'tripcost', (error, decoded)=>{
+                if(error){
+                    return res.json({
+                        sucess: false,
+                        message: 'autentificacion fallida'
+                    });
+                } else{
+                    req.decoded=decoded;
+                    next();
+                }
+            })
+        } else{
+            return res.status(403).send({
+                success: false,
+                message: 'no existe el token'
+            })
+        }
+    })
 
 
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,12 +1,14 @@
 const userController = require("../controllers/usuario")
-module.exports = app =>{
+module.exports = (app) =>{
     app.get("/",(req,res)=>{
         res.send("Enviando");
     });
 
-    app.get("/users",userController.users);
     app.get("/user/:id",userController.user);
     app.post("/createUser",userController.createUser);
+    app.post("/login",userController.login);
+    app.use("",userController.middleware);
+    app.get("/users",userController.users);
 
 
 };
